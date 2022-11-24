9236c6fcdba0fc32f76a8409a0f115e814d2f570
mo <nataleshen.ko@yandex.ru>
2022-11-24 20:24:42 +0300

    commit-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Таблица умножения!");
+        MultiplicationTable multiplicationTable1 = new MultiplicationTable();
+        System.out.println(multiplicationTable1.getTasks().toString());
+
+
+
     }
 }
